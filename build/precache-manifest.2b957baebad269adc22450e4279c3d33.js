b0c4250cdc1b82b54cdcba28e8c3a01c56753cdf
joacobian21@gmail.com <joacobian21@gmail.com>
2021-07-29 12:01:19 -0300

    deolot

---
+++ build/precache-manifest.2b957baebad269adc22450e4279c3d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dad5413ba4eb4f073b98c1cddef98e",
+    "url": "/MyNewWeatherApp/index.html"
+  },
+  {
+    "revision": "9afe3d0c24b41200af9f",
+    "url": "/MyNewWeatherApp/static/css/main.7a30d61b.chunk.css"
+  },
+  {
+    "revision": "4e1f17f447b824043e93",
+    "url": "/MyNewWeatherApp/static/js/2.56962e63.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MyNewWeatherApp/static/js/2.56962e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afe3d0c24b41200af9f",
+    "url": "/MyNewWeatherApp/static/js/main.3424819b.chunk.js"
+  },
+  {
+    "revision": "538c8408cb367f8e5fdd",
+    "url": "/MyNewWeatherApp/static/js/runtime-main.4d7a7e6a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WeatherApp/precache-manifest.6b26d4e4c699efee2979655414631194.js"
+  "/MyNewWeatherApp/precache-manifest.2b957baebad269adc22450e4279c3d33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WeatherApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyNewWeatherApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
